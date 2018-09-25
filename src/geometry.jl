a4e44ceb6702606880774650b9a7adafa82b8bb7
Andrew Ning <aning@byu.edu>
2018-09-24 22:21:25 -0600

    undo some of the visualization separation for now

--- src/geometry.jl
+++ src/geometry.jl
@@ -1,4 +1,4 @@
-
+using PyPlot
 
 function linearsections(xle, yle, zle, chord, theta, npanels, duplicate, spacing)
 
@@ -81,3 +81,44 @@ function simplewing(b, AR, λ, Λ, ϕ, θr, θt, npanels, duplicate, spacing)
 end
 
 
+# TODO: With Pkg3 I can make this a separate module within same repo so base VLM doesn't depending on plotting.
+function visualizegeometry(panels)
+    for i = 1:length(panels)
+        
+        x = [panels[i].rl[1]; panels[i].rr[1]]
+        y = [panels[i].rl[2]; panels[i].rr[2]]
+        z = [panels[i].rl[3]; panels[i].rr[3]]
+        plot3D(x, y, z, color="b")
+        plot3D(x, -y, z, color="b")
+        x = [panels[i].rl[1]; panels[i].rl[1] + 3.0/4*panels[i].chord]
+        y = [panels[i].rl[2]; panels[i].rl[2]]
+        z = [panels[i].rl[3]; panels[i].rl[3]]
+        plot3D(x, y, z, color="b")
+        plot3D(x, -y, z, color="b")
+        x = [panels[i].rr[1]; panels[i].rr[1] + 3.0/4*panels[i].chord]
+        y = [panels[i].rr[2]; panels[i].rr[2]]
+        z = [panels[i].rr[3]; panels[i].rr[3]]
+        plot3D(x, y, z, color="b")
+        plot3D(x, -y, z, color="b")
+        x = [panels[i].rl[1] - 1.0/4*panels[i].chord; panels[i].rr[1] - 1.0/4*panels[i].chord]
+        y = [panels[i].rl[2]; panels[i].rr[2]]
+        z = [panels[i].rl[3]; panels[i].rr[3]]
+        plot3D(x, y, z, color="0.5")
+        plot3D(x, -y, z, color="0.5")
+        x = [panels[i].rl[1] + 3.0/4*panels[i].chord; panels[i].rr[1] + 3.0/4*panels[i].chord]
+        y = [panels[i].rl[2]; panels[i].rr[2]]
+        z = [panels[i].rl[3]; panels[i].rr[3]]
+        plot3D(x, y, z, color="0.5")
+        plot3D(x, -y, z, color="0.5")
+
+        
+    end
+    
+    grid("off")
+    # gca()[:view_init](90.0, 0.0)
+    gca()[:view_init](20, -135)
+    axis("equal")
+    xlabel("x")
+    ylabel("y")
+    zlabel("z")
+end
