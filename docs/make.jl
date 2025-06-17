4506fc69080f14796ee95797bee2c5e260d7bbe0
BTV25 <70768698+BTV25@users.noreply.github.com>
2025-06-16 18:00:23 -0600

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,7 @@
 using Documenter, VortexLattice
 
 makedocs(;
-    format = Documenter.HTML(;size_threshold = 102400, size_threshold_warn = 1020000),
+    format = Documenter.HTML(;size_threshold = 102400, size_threshold_warn = 102000),
     modules = [VortexLattice],
     pages = [
         "Home" => "index.md",
